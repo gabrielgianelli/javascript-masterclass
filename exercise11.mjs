d554da7074eaab2ca0e5e71faca9b3d9e8763333
Gabriel Gianelli <gabrielgianelli@gmail.com>
2022-01-27 20:21:49 -0300

    add exercise11

---
+++ exercise11.mjs
@@ -0,0 +1,15 @@
+import Database from "./database-async.mjs";
+
+const database = new Database();
+database
+    .execute("create table author (id number, name string, age number, city string, state string, country string)")
+    .then(() => 
+        Promise.all([
+            database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
+            database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
+            database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)")
+        ])
+    )
+    .then(() => database.execute("select name, age from author"))
+    .then(result => console.log(JSON.stringify(result, undefined, " ")))
+    .catch(e => console.log(e.message));
